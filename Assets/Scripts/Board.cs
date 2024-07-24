63760762e0e8ecead7c1a2c8389ba3f304deda7b
M  .  Khodaverdi <142523619+Muhammadkhodaverdi@users.noreply.github.com>
2024-07-24 17:12:39 +0330

    initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [Header("Refrences")]
+    [SerializeField] private GameObject refrence;
+
+    [Header("Attributes")]
+    [SerializeField] private GameObject attribute;
+}
