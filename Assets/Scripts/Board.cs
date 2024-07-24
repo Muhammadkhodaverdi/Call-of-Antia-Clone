8975dd66805b372670ae238bce9663de15228765
M  .  Khodaverdi <142523619+Muhammadkhodaverdi@users.noreply.github.com>
2024-07-24 17:32:25 +0330

    Board Script

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
     [Header("Refrences")]
-    [SerializeField] private GameObject refrence;
+    [SerializeField] private GameObject tilePrefab;
 
     [Header("Attributes")]
-    [SerializeField] private GameObject attribute;
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+
+    private Tile[,] allTiles;
+
+    private void Start()
+    {
+        allTiles = new Tile[width, height];
+        Init();
+    }
+
+    private void Init()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Debug.Log($"({i},{j})");
+
+                Vector2 tempPosition = new Vector2(i, j);
+
+                GameObject tileGameObject = Instantiate(tilePrefab, tempPosition, Quaternion.identity, transform);
+            }
+        }
+    }
 }
